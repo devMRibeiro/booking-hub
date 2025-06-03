8e83f904439c54df18592e1478dda966ee1a52f3
devMRibeiro <michael.douglas2020@hotmail.com>
2025-06-02 23:59:46 -0300

    refactor: return RoomResponseDTO instead of Room in list endpoint

--- src/main/java/com/github/devmribeiro/bookinghub/controller/RoomController.java
+++ src/main/java/com/github/devmribeiro/bookinghub/controller/RoomController.java
@@ -1,13 +1,17 @@
 package com.github.devmribeiro.bookinghub.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.devmribeiro.bookinghub.dto.room.RoomResponseDTO;
 import com.github.devmribeiro.bookinghub.model.Room;
 import com.github.devmribeiro.bookinghub.service.RoomService;
 
@@ -22,7 +26,20 @@ public class RoomController {
 	}
 
 	@GetMapping("/list")
-	public @ResponseBody List<Room> listByType(@RequestParam(value = "type", required = false) String type) {
-		return service.list(type);
+	@ResponseBody
+	public ResponseEntity<List<RoomResponseDTO>> list(
+			@RequestParam(value = "capacity", required = false) Integer capacity,
+			@RequestParam(value = "type", required = false) String type) {
+
+		List<Room> result = service.list(capacity, type);
+		
+	    if (result == null || result.isEmpty())
+	        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		
+	    List<RoomResponseDTO> response = result.stream()
+	            .map(RoomResponseDTO::new)
+	            .collect(Collectors.toList());
+
+	    return ResponseEntity.ok(response);
 	}
 }
