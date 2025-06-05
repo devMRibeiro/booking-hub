e89072520915c2e61925426debe94892ac03d25e
devMRibeiro <michael.douglas2020@hotmail.com>
2025-06-04 23:59:07 -0300

    refactor: customize query to list only available rooms

--- src/main/java/com/github/devmribeiro/bookinghub/repository/RoomRepository.java
+++ src/main/java/com/github/devmribeiro/bookinghub/repository/RoomRepository.java
@@ -3,13 +3,20 @@ package com.github.devmribeiro.bookinghub.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.github.devmribeiro.bookinghub.model.Room;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
-	List<Room> findByType(String type);
-	List<Room> findByCapacity(Integer capacity);
-	List<Room> findByTypeAndCapacity(String type, Integer capacity);
+	@Query(value = "select r.room_id, r.hotel_id, r.capacity, r.label, r.type from room r left join booking b on b.room_id = r.room_id where b.room_id is null and r.type = :type", nativeQuery = true)
+	List<Room> findAvailableByType(@Param("type") String type);
+
+	@Query(value = "select r.room_id, r.hotel_id, r.capacity, r.label, r.type from room r left join booking b on b.room_id = r.room_id where b.room_id is null and r.capacity = :capacity", nativeQuery = true)
+	List<Room> findByCapacity(@Param("capacity") Integer capacity);
+
+	@Query(value = "select r.room_id, r.hotel_id, r.capacity, r.label, r.type from room r left join booking b on b.room_id = r.room_id where b.room_id is null and r.type = :type and r.capacity = :capacity", nativeQuery = true)
+	List<Room> findByTypeAndCapacity(@Param("type") String type, @Param("capacity") Integer capacity);
 }
