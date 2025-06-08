f5e7bcc483cbe4a602650e6585211ca6f6eb70fe
devMRibeiro <michael.douglas2020@hotmail.com>
2025-06-08 16:58:42 -0300

    feat: created endpoint to save a new room

--- src/main/java/com/github/devmribeiro/bookinghub/controller/RoomController.java
+++ src/main/java/com/github/devmribeiro/bookinghub/controller/RoomController.java
@@ -1,16 +1,20 @@
 package com.github.devmribeiro.bookinghub.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.devmribeiro.bookinghub.dto.room.RoomCreateDTO;
 import com.github.devmribeiro.bookinghub.dto.room.RoomResponseDTO;
 import com.github.devmribeiro.bookinghub.model.Room;
 import com.github.devmribeiro.bookinghub.service.RoomService;
@@ -24,7 +28,7 @@ public class RoomController {
 	public RoomController(RoomService service) {
 		this.service = service;
 	}
-
+	
 	@GetMapping("/list")
 	public @ResponseBody ResponseEntity<List<RoomResponseDTO>> list(
 			@RequestParam(value = "capacity", required = false) Integer capacity,
@@ -40,5 +44,15 @@ public class RoomController {
 	            .collect(Collectors.toList());
 
 	    return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<RoomResponseDTO> create(@RequestBody RoomCreateDTO dto) {
+	    Room createdRoom = service.create(dto.toEntity(dto));
+	    RoomResponseDTO responseDTO = new RoomResponseDTO(createdRoom);
+	    URI location = URI.create("/rooms/" + createdRoom.getRoomId());
+	    return ResponseEntity
+	            .created(location)
+	            .body(responseDTO);
 	}
 }
--- src/main/java/com/github/devmribeiro/bookinghub/service/RoomService.java
+++ src/main/java/com/github/devmribeiro/bookinghub/service/RoomService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.github.devmribeiro.bookinghub.exception.InvalidInputException;
 import com.github.devmribeiro.bookinghub.model.Room;
 import com.github.devmribeiro.bookinghub.repository.RoomRepository;
 
@@ -27,5 +28,21 @@ public class RoomService {
 			return repository.findByType(type);
 
 		return repository.findAll(); 
+	}
+
+	public Room create(Room room) {
+		if (room == null)
+			throw new InvalidInputException("Fill in the fields correctly");
+
+		if (room.getLabel() == null || room.getLabel().isBlank())
+			throw new InvalidInputException("Label field connot be empty");
+
+		if (room.getType() == null || room.getType().isBlank())
+			throw new InvalidInputException("Type field connot be empty");
+
+		if (room.getCapacity() == null || room.getCapacity() < 1)
+			throw new InvalidInputException("Capacity field connot be empty or less than 1");
+
+		return repository.save(room);
 	}
 }
