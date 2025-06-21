632e094053302112c029bbbb66675827e06901f4
devMRibeiro <michael.douglas2020@hotmail.com>
2025-06-21 15:39:30 -0300

    feat: implements edit method to serve controller

--- src/main/java/com/github/devmribeiro/bookinghub/service/RoomService.java
+++ src/main/java/com/github/devmribeiro/bookinghub/service/RoomService.java
@@ -45,14 +45,42 @@ public class RoomService {
 			throw new InvalidInputException("Fill in the fields correctly");
 
 		if (room.getLabel() == null || room.getLabel().isBlank())
-			throw new InvalidInputException("Label field connot be empty");
+			throw new InvalidInputException("Label field cannot be empty");
 
 		if (room.getType() == null || room.getType().isBlank())
-			throw new InvalidInputException("Type field connot be empty");
+			throw new InvalidInputException("Type field cannot be empty");
 
 		if (room.getCapacity() == null || room.getCapacity() < 1)
-			throw new InvalidInputException("Capacity field connot be empty or less than 1");
+			throw new InvalidInputException("Capacity field cannot be empty or less than 1");
 
 		return repository.save(room);
+	}
+	
+	public Room edit(Integer roomId, Room room) {
+		if (roomId == null || roomId < 1)
+			throw new InvalidInputException("Invalid Room ID");
+
+		if (room == null)
+			throw new InvalidInputException("Fill in the fields correctly");
+
+		if (room.getLabel() == null || room.getLabel().isBlank())
+			throw new InvalidInputException("Label field cannot be empty");
+
+		if (room.getType() == null || room.getType().isBlank())
+			throw new InvalidInputException("Type field cannot be empty");
+
+		if (room.getCapacity() == null || room.getCapacity() < 1)
+			throw new InvalidInputException("Capacity field cannot be empty or less than 1");
+
+		Room originalRoom = repository.findByRoomId(roomId);
+
+		if (originalRoom == null)
+			throw new ResourceNotFoundException("Room with ID " + roomId + " not found");
+
+		originalRoom.setLabel(room.getLabel());
+		originalRoom.setType(room.getType());
+		originalRoom.setCapacity(room.getCapacity());
+
+		return repository.save(originalRoom);
 	}
 }
